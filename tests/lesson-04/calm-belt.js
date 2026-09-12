9e4612458dc8081d48514925f3a65974294a5da7
test 02 - Anh Nguyen <nguyenanh1904.vn@gmail.com>
2026-09-12 19:38:07 +0700

    feat: add solution for calm belt challenge

---
+++ tests/lesson-04/calm-belt.js
@@ -0,0 +1,10 @@
+let navigatorName = "Luffy";
+let currentHakiLevel = 3;
+const islandA = 15;
+const islandB = 25;
+const islandC = 40;
+
+const sum = islandA + islandB + islandC;
+const avg = sum / 3;
+const remainder = sum % 3;
+console.log(remainder);
