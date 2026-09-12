25a56bf2943a9cf591ff79165e7ffad08195ffef
test 02 - Anh Nguyen <nguyenanh1904.vn@gmail.com>
2026-09-12 19:44:08 +0700

    feat: add solution for calm belt challenge part 2

---
+++ tests/lesson-04/calm-belt-2.js
@@ -0,0 +1,15 @@
+const devilFruit = "gomu-gomu";
+let awakeningStatus;
+
+if (devilFruit === "gomu-gomu") {
+  awakeningStatus = "Gear 5 Unlocked!";
+} else if (devilFruit === "mera-mera") {
+  awakeningStatus = "Flame Emperor!";
+} else if (devilFruit === "ope-ope") {
+  awakeningStatus = "Room & Ope!";
+} else if (devilFruit === "none") {
+  awakeningStatus = "Normal Human";
+} else {
+  awakeningStatus = "Unknown Devil Fruit";
+}
+console.log(awakeningStatus);
